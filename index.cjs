1b4061209b40e473eb1ad74e167ff55d731e94ad
elkan1788 <elkan1788@139.com>
2026-02-23 18:23:47 +0800

    :zap: Add debug info.

--- index.cjs
+++ index.cjs
@@ -4,6 +4,9 @@ module.exports = Application({
   region: false,   // 关闭内置解析
 
   async postSave(comment) {
+
+    console.log('postSave: ', comment);
+
     try {
       if (comment.ip) {
         const res = await fetch(`http://ip-api.com/json/${comment.ip}?fields=61439`);
@@ -13,6 +16,8 @@ module.exports = Application({
     } catch (e) {
       console.error('IP resolve failed:', e);
     }
+
+    console.log('postSave after IP resolve: ', comment);
 
     return comment;
   },
